8a6ac5a831589565896be68170ea62611d478b8b
Ricardo A <99375284+sulf0nic@users.noreply.github.com>
2024-06-17 19:16:46 -0600

    ChristmasTree.java

--- ChristmasTree.java
+++ ChristmasTree.java
@@ -2,9 +2,10 @@ import java.util.Random;
 
 public class ChristmasTree {
     public static void main(String[] args) {
+        
         int height = 10; // Altura fija del árbol de Navidad
         Random random = new Random();
-        
+        System.out.print("Programa de arbol de navidad ")
         // Dibujar la parte superior del árbol
         for (int i = 1; i <= height; i++) {
             for (int j = height; j > i; j--) {
