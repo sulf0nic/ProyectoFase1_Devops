b5489175a4f7f0a4fec19505c44676e755401b76
sulf0nic <rd.arenas@ebc.edu.mx>
2024-07-09 20:36:13 -0600

    pruebas

--- provider.tf
+++ provider.tf
@@ -3,12 +3,9 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
+      region = "us-east-2c"  
     }
   }
   required_version = ">= 1.0"
-  
-provider "aws" {
-  region = "us-east-2c"  
-}
   
 }
