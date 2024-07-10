d5393d958856948b569e1c2a1c26d313591092a7
sulf0nic <rd.arenas@ebc.edu.mx>
2024-07-09 21:41:33 -0600

    Cambio de par de llaves

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "exampleF3" {
   ami           = "ami-06c68f701d8090592"  # ID de la AMI que se utilizará
   instance_type = "t2.micro"              # Tipo de instancia EC2
   subnet_id     = "subnet-05461e737d22d8649"  # ID de la subred donde se lanzará la instancia
-  key_name      = "Fase3"                  # Nombre de la clave SSH para acceder a la instancia
+  key_name      = "Produccion"                  # Nombre de la clave SSH para acceder a la instancia
 
   tags = {
     Terraform   = "true"   # Etiqueta para indicar que la instancia fue creada con Terraform
